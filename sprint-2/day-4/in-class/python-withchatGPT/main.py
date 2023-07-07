c052647acf003b3309fce0999386d23bc6198762
Shivam Chaudhary <shivamchaudhary75@gmail.com>
2023-07-07 22:05:32 +0530

    python flask

---
+++ sprint-2/day-4/in-class/python-withchatGPT/main.py
@@ -0,0 +1,164 @@
+# print("Rectangle Area Calculator")
+# x= int(input("Length\n "))
+# y= int(input("Breadth?\n"))
+
+# area = x*y 
+
+# print("Area is" , area)
+
+
+# numbers = [1, 2, 3, 4, 5]
+# for num in numbers:
+#     print(num)
+
+
+
+
+# name = input("What is Your Name?\n")
+# age = input("How old are you?\n")
+# color = input("What is your favourite color?\n")
+
+# print(f"Your name is: {name}\nYour age is: {age}\nYour favorite color is: {color}")
+
+
+# Exercise:
+
+# Create a variable num1 and assign it an integer value.
+# Create a variable num2 and assign it a float value.
+# Create a variable name and assign it your name as a string.
+# Create a list fruits containing the following fruits: "apple", "banana", "orange".
+# Create a tuple coordinates containing latitude and longitude values (e.g., 37.7749, -122.4194).
+# Create a dictionary person with the following key-value pairs:
+# Key: "name", Value: Your name as a string.
+# Key: "age", Value: Your age as an integer.
+# Key: "city", Value: Your city of residence as a string.
+# After completing the exercise, let me know, and we'll discuss the solutions and move on to the next subtopic: Operators.
+
+
+# num1 = 2
+# num2 = 1.5
+# name = "Shivam"
+# fruits = ["apple", "banana", "orange"]
+# coordinates = (37.7749, -122.4194)
+# person = {
+#     "name": "John Doe",
+#     "age": 25,
+#     "city": "New York"
+# }
+
+
+
+# Create two variables, num1 and num2, and assign them any numeric values of your choice.
+
+# Perform arithmetic operations (addition, subtraction, multiplication, division) using these variables and display the results.
+# Use at least one comparison operator to compare the values of num1 and num2 and display the result.
+# Create a variable is_raining and assign it a boolean value indicating whether it is currently raining or not.
+
+# Create a variable is_sunny and assign it a boolean value indicating whether it is currently sunny or not.
+# Use logical operators to determine if it is both raining and sunny, or if it is either raining or sunny, and display the results.
+
+
+# num1 = 60
+# num2 = 25
+
+# add = num1+num2
+# print("Addition:" , add)
+
+# minus = num1-num2
+# print("Substraction" , minus)
+
+# multiply = num1*num2
+# print("Multiply" , multiply)
+
+# division = num1/num2 
+# print("Division" , division)
+
+# compare = add > minus
+# print(compare)
+
+# is_raining = False
+# is_sunny = True
+
+# result = is_raining == is_sunny 
+# print(result)
+
+# Exercise:
+
+# Write a Python script that prompts the user to enter an integer. If the number is positive, display a message saying "Number is positive." If the number is negative, display a message saying "Number is negative." If the number is zero, display a message saying "Number is zero."
+
+# Write a Python script using a loop that calculates the sum of all numbers from 1 to 10 (inclusive) and displays the result.
+
+# Take your time to solve these exerci
+
+# num = int(input("Enter an integer\n"))
+
+# if num > 0 :
+#     print("Number is positive.")
+# elif num < 0:
+#     print("Number is negative.")
+# else:
+#     print("Number is zero.")
+    
+    
+
+# count = 1
+# sum = 0
+
+# while count < 11:
+#     sum+= count
+#     count += 1
+
+# print(sum)
+
+
+# def calculate_average(list):
+#     sum = 0
+#     n = 0
+#     for k in list:
+#         sum += k 
+#         n+=1
+#     average = sum/n 
+#     return average
+
+
+# list1 = [1,2,3,4,5,6,7,8,9,10]
+# print(calculate_average(list1))
+
+# import math 
+
+# number = float(input("Number\n"))
+
+# square_root = round(math.sqrt(number),2)
+
+# print("The square root of", number, "is", square_root)
+
+
+# # Opening a file in write mode
+# file = open("example.txt", "w")
+
+# # Writing data to the file
+# file.write("Hello, World!\n")
+# file.write("This is a sample file.\n")
+
+# # Closing the file
+# file.close()
+
+# # Opening the same file in read mode
+# file = open("example.txt", "r")
+
+# # Reading the contents of the file
+# content = file.read()
+# print(content)
+
+# # Closing the file
+# file.close()
+
+
+
+# FILE SYSTEM 
+# FILE SYSTEM 
+# FILE SYSTEM 
+# FILE SYSTEM 
+
+
+
