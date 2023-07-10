956786a507b5acf200d7d6bab4993853b4693334
Shivam Chaudhary <shivamchaudhary75@gmail.com>
2023-07-10 17:58:52 +0530

    eval ques

--- sprint-3/eval/main.py
+++ sprint-3/eval/main.py
@@ -1,4 +1,6 @@
 
+# PART-1
+
 str = input("Give me the String\n")
 
 def reverse_string(str):
@@ -8,3 +10,24 @@ def reverse_string(str):
 reverse_string(str)
 
 
+# PART-2 
+
+def max_salary_employee(employees):
+    max_salary = 0
+    employee = None
+    
+    for i in employees:
+        if i["salary"] > max_salary:
+            max_salary = i["salary"]
+            employee = i
+    
+    return employee
+
+employees = [
+    {"name": "John", "salary": 3000, "designation": "developer"},
+    {"name": "Emma", "salary": 4000, "designation": "developer"},
+    {"name": "Kelly", "salary": 3500, "designation": "developer"}
+]
+
+res = max_salary_employee(employees)
+print(res)
