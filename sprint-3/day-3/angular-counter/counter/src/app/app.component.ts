f95d3a558ee64ab76c84bfbd319af96736e55d5c
Shivam Chaudhary <shivamchaudhary75@gmail.com>
2023-07-24 22:11:15 +0530

    changes

--- sprint-3/day-3/angular-counter/counter/src/app/app.component.ts
+++ sprint-3/day-3/angular-counter/counter/src/app/app.component.ts
@@ -15,3 +15,21 @@ export class AppComponent {
     this.count--;
   }
 }
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css'],
+// })
+// export class AppComponent {
+//   title = 'counter';
+//   count = 0;
+//   counter() {
+//     this.count++;
+//   }
+//   decre() {
+//     this.count--;
+//   }
+// }
